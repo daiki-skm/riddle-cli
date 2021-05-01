c138cb6a93e54c39f51b599eae260843eed168ea
佐久間大空 <daikisakuma6040@gmail.com>
2021-05-01 21:05:18 +0900

    update: cli

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("aaa")
-	fmt.Println("aaa")
+	app := cli.NewApp()
+	app.Name = "greet"
+	app.Usage = "Give a good greeting."
+	app.Version = "0.0.1"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Hello!!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
