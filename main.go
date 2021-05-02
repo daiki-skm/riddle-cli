783408d111d6c8a2150c61db15a4304d29061bb3
佐久間大空 <daikisakuma6040@gmail.com>
2021-05-02 16:58:28 +0900

    update: demo

--- main.go
+++ main.go
@@ -2,114 +2,120 @@ package main
 
 import (
 	"fmt"
+	"github.com/urfave/cli"
 	"log"
 	"os"
-
-	"github.com/urfave/cli"
 )
 
 func main() {
 	app := cli.NewApp()
-	app.Name = "quiz"
-	app.Usage = "Give a quiz."
+	app.Name = "Riddle"
+	app.Usage = "Give a Riddle."
 	app.Version = "0.0.1"
+
+	var riddleNum string
+	var ansNum string
+
+	//app.Flags = []cli.Flag {
+	//	&cli.StringFlag{
+	//		Name: "riddle",
+	//		Usage: "Give a riddle.",
+	//		Destination: &num,
+	//	},
+	//	&cli.StringFlag{
+	//		Name: "answer",
+	//		Usage: "Give a answer.",
+	//		Destination: &ansnum,
+	//	},
+	//}
+
+	//app.Action = func(c *cli.Context) error {
+	//	return nil
+	//}
 
 	app.Commands = []*cli.Command{
 		{
-			Name:    "quiz",
-			Aliases: []string{"q"},
-			Usage:   "Give a quiz",
-			Subcommands: []*cli.Command{
-				// `go run main.go quiz 1 でactionするコマンド
-				{
-					Name:  "1",
-					Usage: "quiz 1",
-					Action: func(c *cli.Context) error {
-						fmt.Println("1+1=?")
-						return nil
-					},
-				},
-				// `go run main.go quiz 2 でactionするコマンド
-				{
-					Name:  "2",
-					Usage: "quiz 2",
-					Action: func(c *cli.Context) error {
-						fmt.Println("2+2=?")
-						return nil
-					},
+			Name:    "riddle",
+			Aliases: []string{"r"},
+			Usage:   "riddle --num 1 (Choose a number from 1 to 9.)",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:        "num",
+					Usage:       "--num 1 (Choose a number from 1 to 9.)",
+					Destination: &riddleNum,
 				},
 			},
-		},
-		{
-			Name:    "MyAnswer",
-			Aliases: []string{"m"},
-			Usage:   "Write a your answer",
-			Subcommands: []*cli.Command{
-				// `go run main.go MyAnswer 1 --text `val` でactionするコマンド
-				{
-					Name:  "1",
-					Usage: "MyAnswer 1",
-					Flags: []cli.Flag{
-						&cli.StringFlag{
-							Name:  "text",
-							Usage: "true / false",
-						},
-					},
-					Action: func(c *cli.Context) error {
-						//fmt.Printf("%s\n", c.String("text"))
-						if c.String("text") == "2" {
-							fmt.Println("True!")
-						} else {
-							fmt.Println("False!")
-						}
-						return nil
-					},
-				},
-				// `go run main.go MyAnswer 2 --text `val` でactionするコマンド
-				{
-					Name:  "2",
-					Usage: "MyAnswer 2",
-					Flags: []cli.Flag{
-						&cli.StringFlag{
-							Name:  "text",
-							Usage: "true / false",
-						},
-					},
-					Action: func(c *cli.Context) error {
-						//fmt.Printf("%s\n", c.String("text"))
-						if c.String("text") == "4" {
-							fmt.Println("True!")
-						} else {
-							fmt.Println("False!")
-						}
-						return nil
-					},
-				},
+			Action: func(c *cli.Context) error {
+				fmt.Printf("Problem: ")
+				switch riddleNum {
+				case "1":
+					fmt.Println("Why was ６ afraid of 7?")
+				case "2":
+					fmt.Println("If you say it you break it. What is it?")
+				case "3":
+					fmt.Println("What letter of the alphabet has the most water?")
+				case "4":
+					fmt.Println("What flowers grow under your nose?")
+				case "5":
+					fmt.Println("What can you catch but not throw?")
+				case "6":
+					fmt.Println("What is a snake’s favorite subject?")
+				case "7":
+					fmt.Println("What state is round at both ends, and high in the middle?")
+				case "8":
+					fmt.Println("What starts with T, ends with T, and is full of T?")
+				case "9":
+					fmt.Println("I’m tall when I’m young and I’m short when I’m old. What am I?")
+				default:
+					fmt.Println("Please choose from 1 to 9.")
+				}
+				return nil
 			},
 		},
 		{
 			Name:    "answer",
 			Aliases: []string{"a"},
-			Usage:   "Give an answer",
-			Subcommands: []*cli.Command{
-				// `go run main.go answer 1 でactionするコマンド
-				{
-					Name:  "1",
-					Usage: "answer 1",
-					Action: func(c *cli.Context) error {
-						fmt.Println("2")
-						return nil
-					},
+			Usage:   "answer --num 1 (Choose a number from 1 to 9.)",
+			Flags: []cli.Flag{
+				&cli.StringFlag{
+					Name:        "num",
+					Usage:       "--num 1 (Choose a number from 1 to 9.)",
+					Destination: &ansNum,
 				},
-				// `go run main.go answer 2 でactionするコマンド
-				{
-					Name:  "2",
-					Usage: "answer 2",
-					Action: func(c *cli.Context) error {
-						fmt.Println("4")
-						return nil
-					},
-				},
+			},
+			Action: func(c *cli.Context) error {
+				switch ansNum {
+				case "1":
+					fmt.Println("Answer: Because ７８９！")
+					fmt.Println("Reason: 7 8(ate) 9")
+				case "2":
+					fmt.Println("Answer: Silence")
+					fmt.Println("Reason: break silence")
+				case "3":
+					fmt.Println("Answer: C")
+					fmt.Println("Reason: C()Sea")
+				case "4":
+					fmt.Println("Answer: Fire")
+					fmt.Println("Reason: Fire grows as we eat (burn) things, but it goes out when water is poured on it.")
+				case "5":
+					fmt.Println("Answer: A cold")
+					fmt.Println("Reason: We use 'Catch a cold', but we don't use 'Throw a cold'.")
+				case "6":
+					fmt.Println("Answer: HISStory!")
+					fmt.Println("Reason: The hissing sound made by a snake is called 'hiss' in English. The word 'his' in 'history' and the sound of the snake 'hiss' make 'HISStory'.")
+				case "7":
+					fmt.Println("Answer: Ohio")
+					fmt.Println("Reason: 'O''hi'(high)'o'")
+				case "8":
+					fmt.Println("Answer: Teapot")
+					fmt.Println("Reason: 'Teapot' is spelled starting with a 'T' and ending with a 'T'.'T' and 'Tea'")
+				case "9":
+					fmt.Println("Answer: Candle")
+					fmt.Println("Reason: The more you use a candle, the shorter it becomes.")
+				default:
+					fmt.Println("Please choose from 1 to 9.")
+				}
+				return nil
 			},
 		},
 	}
